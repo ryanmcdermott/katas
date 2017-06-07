70bef57629ad19d93096ccbdd4846544ecb3de06
Ryan McDermott <ryantmcdermott@gmail.com>
2017-06-06 19:50:25 -0700

    Move graph to unordered_map

--- graph/graph.hpp
+++ graph/graph.hpp
@@ -6,16 +6,14 @@
 using std::list;
 
 template <class T> class Graph {
-  using Edge = std::pair<T, T>;
-  using EdgeList = std::list<Edge>;
+  using EdgeList = std::list<T>;
   using AdjacencyList = std::unordered_map<T, EdgeList>;
 
 private:
-  int vertex_count;
   AdjacencyList *adj;
 
 public:
-  Graph(int count);
+  Graph();
   ~Graph();
   void add_edge(T source, T destination);
   bool is_path_between(T source, T destination);
--- graph/main.cpp
+++ graph/main.cpp
@@ -1,14 +1,18 @@
-#include "./graph.hpp"
+#include "./graph.cpp"
+#include <assert.h>
 #include <iostream>
 
 using std::cout;
 
 int main() {
-  Graph<int> g(4);
+  Graph<int> g;
   g.add_edge(0, 1);
   g.add_edge(0, 2);
   g.add_edge(1, 2);
   g.add_edge(2, 0);
   g.add_edge(2, 3);
   g.add_edge(3, 3);
+
+  bool path_betweeen = g.is_path_between(0, 3);
+  assert(path_betweeen == true);
 }
