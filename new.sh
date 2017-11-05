9cae36c3ff17992ede8dcc91918e89933c3646a9
Ryan McDermott <ryantmcdermott@gmail.com>
2017-11-04 20:05:24 -0700

    Fix new.sh

--- new.sh
+++ new.sh
@@ -5,7 +5,7 @@ cp template_algorithm.cpp $FOLDER/$FOLDER.cpp
 cp template_algorithm.hpp $FOLDER/$FOLDER.hpp
 
 cd $FOLDER
-sed -i -e 's/ALGORITHM/$FOLDER/g' ./main.cpp
-sed -i -e 's/ALGORITHM/$FOLDER/g' ./$FOLDER.cpp
-sed -i -e 's/ALGORITHM/$FOLDER/g' ./$FOLDER.hpp
+sed -i -e "s/ALGORITHM/$FOLDER/g" ./main.cpp
+sed -i -e "s/ALGORITHM/$FOLDER/g" ./$FOLDER.cpp
+sed -i -e "s/ALGORITHM/$FOLDER/g" ./$FOLDER.hpp
 cd ../
