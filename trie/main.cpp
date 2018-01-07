575fcb8b351176f20e0ac5a3ce5b520d7ed9bc12
Ryan McDermott <ryantmcdermott@gmail.com>
2018-01-06 17:50:33 -0800

    Trie start

--- trie/main.cpp
+++ trie/main.cpp
@@ -1,7 +1,15 @@
 #include "./trie.hpp"
 #include <assert.h>
 #include <iostream>
+#include <string>
 
 using std::cout;
+using std::string;
 
-int main() {}
+int main() {
+  string keys[] = {"freedom", "free", "freed", "trial", "judgment", "fury"};
+  struct TrieNode *root = getTrieNode();
+  for (auto &key : keys) {
+    insert(root, key);
+  }
+}
