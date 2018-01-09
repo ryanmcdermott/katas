fd04493647b025c7d5723331c3c36adb63f494e9
Ryan McDermott <ryantmcdermott@gmail.com>
2018-01-08 20:05:10 -0800

    Trie header stubs

--- trie/trie.hpp
+++ trie/trie.hpp
@@ -1,6 +1,17 @@
 #ifndef trie_h_
 #define trie_h_
 
-void trie();
+const ALPHABET_SIZE = 26;
+
+struct TrieNode {
+  struct *TrieNode children[ALPHABET_SIZE];
+  bool isEnd;
+};
+
+void insert(TrieNode *root, string key);
+
+struct TrieNode *getNode();
+
+bool search(TrieNode *root, string key);
 
 #endif
