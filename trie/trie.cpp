10527e3768cc6b8573c8ba04b51f5d0e9c863821
Ryan McDermott <ryantmcdermott@gmail.com>
2018-01-10 21:19:28 -0800

    Trie create node

--- trie/trie.cpp
+++ trie/trie.cpp
@@ -1,6 +1,19 @@
 #include "./trie.hpp"
+#include <string>
+
+using std::string;
+
 void insert(TrieNode *root, string key) {}
 
-struct TrieNode *getNode() {}
+struct TrieNode *createNode() {
+  struct TrieNode *root = new TrieNode();
+  root->isEnd = false;
+
+  for (int i = 0; i < ALPHABET_SIZE; i++) {
+    root->children[i] = 0;
+  }
+
+  return root;
+}
 
 bool search(TrieNode *root, string key) {}
--- trie/trie.hpp
+++ trie/trie.hpp
@@ -1,16 +1,19 @@
 #ifndef trie_h_
 #define trie_h_
+#include <string>
 
-const ALPHABET_SIZE = 26;
+using std::string;
+
+const int ALPHABET_SIZE = 26;
 
 struct TrieNode {
-  struct *TrieNode children[ALPHABET_SIZE];
+  struct TrieNode *children[ALPHABET_SIZE];
   bool isEnd;
 };
 
 void insert(TrieNode *root, string key);
 
-struct TrieNode *getNode();
+struct TrieNode *createNode();
 
 bool search(TrieNode *root, string key);
 
