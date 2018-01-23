f9a4f63a793ac9223417873bfffff8cb75364419
Ryan McDermott <ryantmcdermott@gmail.com>
2018-01-23 07:05:38 -0800

    Sorted rotated array method

--- sorted_rotated_array/sorted_rotated_array.cpp
+++ sorted_rotated_array/sorted_rotated_array.cpp
@@ -1,2 +1,26 @@
 #include "./sorted_rotated_array.hpp"
-int sorted_rotated_array(int arr[], int low, int high, int key) {}
+int sorted_rotated_array(int arr[], int low, int high, int key) {
+  if (low > high) {
+    return -1;
+  }
+
+  int mid = (low + high) / 2;
+
+  if (arr[mid] == key) {
+    return mid;
+  }
+
+  if (arr[low] <= arr[mid]) {
+    if (key >= arr[low] && key <= arr[mid]) {
+      return sorted_rotated_array(arr, low, mid - 1, key);
+    }
+
+    return sorted_rotated_array(arr, mid + 1, high, key);
+  } else {
+    if (key >= arr[mid] && key <= arr[high]) {
+      return sorted_rotated_array(arr, mid + 1, high, key);
+    }
+
+    return sorted_rotated_array(arr, low, mid - 1, key);
+  }
+}
