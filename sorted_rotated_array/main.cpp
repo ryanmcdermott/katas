447b3adf1e8b2dbe1cfc964b3201ac883e9428bd
Ryan McDermott <ryantmcdermott@gmail.com>
2018-01-24 07:32:14 -0800

    Sorted rotated array tests

--- sorted_rotated_array/main.cpp
+++ sorted_rotated_array/main.cpp
@@ -1,7 +1,16 @@
-#include "./sorted_rotated_array.hpp"
 #include <assert.h>
 #include <iostream>
+#include "./sorted_rotated_array.hpp"
 
 using std::cout;
 
-int main() {}
+int main() {
+  int arr[] = {4, 5, 6, 7, 8, 9, 1, 2, 3};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  int key = 6;
+
+  int result;
+  result = sorted_rotated_array(arr, 0, n - 1, key);
+
+  assert(result == 2);
+}
