fcd59d7dd730f7758321d30c70803083fb794ad1
Ryan McDermott <ryantmcdermott@gmail.com>
2018-01-27 18:07:30 -0800

    Linked list remove duplicates method

--- linked_list_remove_duplicates/linked_list_remove_duplicates.cpp
+++ linked_list_remove_duplicates/linked_list_remove_duplicates.cpp
@@ -1,2 +1,31 @@
 #include "./linked_list_remove_duplicates.hpp"
-void linked_list_remove_duplicates(Node *head) {}
+
+void push(Node **head, int data) {
+  struct Node *new_node = new Node;
+
+  new_node->data = data;
+  new_node->next = (*head);
+  (*head) = new_node;
+}
+
+void linked_list_remove_duplicates(Node *head) {
+  Node *current = head;
+
+  Node *next;
+
+  if (current == 0) {
+    return;
+  } else {
+    next = current->next;
+  }
+
+  while (current->next != 0) {
+    if (current->data == current->next->data) {
+      next = current->next->next;
+      delete current;
+      current = next;
+    } else {
+      current = current->next;
+    }
+  }
+}
