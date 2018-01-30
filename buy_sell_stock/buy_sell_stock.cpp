b7104a0276ae23cb85026983e6786326d7dc65c6
Ryan McDermott <ryantmcdermott@gmail.com>
2018-01-29 22:11:02 -0800

    Buy sell stock algorithm

--- buy_sell_stock/buy_sell_stock.cpp
+++ buy_sell_stock/buy_sell_stock.cpp
@@ -1,2 +1,20 @@
 #include "./buy_sell_stock.hpp"
-void buy_sell_stock() {}
+#include <algorithm>
+#include <limits>
+#include <vector>
+
+using std::vector;
+using std::numeric_limits;
+using std::min;
+using std::max;
+
+int buy_sell_stock(vector<int> &prices) {
+  int current_min_price = numeric_limits<int>::max();
+  int max_profit = 0;
+
+  for (auto &price : prices) {
+    int max_profit_now = price - current_min_price;
+    max_profit = max(max_profit_now, max_profit);
+    current_min_price = min(current_min_price, price);
+  }
+}
