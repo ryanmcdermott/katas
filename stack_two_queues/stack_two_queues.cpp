f55e5d23862024f9614615c5de6f0557c177b726
Ryan McDermott <ryantmcdermott@gmail.com>
2018-02-04 20:45:51 -0800

    Stack two queues pop

--- stack_two_queues/stack_two_queues.cpp
+++ stack_two_queues/stack_two_queues.cpp
@@ -3,12 +3,27 @@
 
 using std::queue;
 class stack_two_queues {
-  queue<int> q1, q1;
+  queue<int> q1, q2;
 
  public:
   stack_two_queues() {}
 
-  void pop() {}
+  void pop() {
+    if (q1.empty()) {
+      return;
+    }
+
+    while (q1.size() != 1) {
+      q2.push(q1.front());
+      q1.pop();
+    }
+
+    q1.pop();
+
+    queue<int> temp = q1;
+    q1 = q2;
+    q2 = temp;
+  }
 
   void push(int num) {}
 
