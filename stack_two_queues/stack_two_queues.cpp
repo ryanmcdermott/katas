e24aee5c16d827c869004fe37b0b192869337403
Ryan McDermott <ryantmcdermott@gmail.com>
2018-02-05 22:03:58 -0800

    Stack two queues top

--- stack_two_queues/stack_two_queues.cpp
+++ stack_two_queues/stack_two_queues.cpp
@@ -2,30 +2,38 @@
 #include <queue>
 
 using std::queue;
-class stack_two_queues {
-  queue<int> q1, q2;
 
- public:
-  stack_two_queues() {}
-
-  void pop() {
-    if (q1.empty()) {
-      return;
-    }
-
-    while (q1.size() != 1) {
-      q2.push(q1.front());
-      q1.pop();
-    }
-
-    q1.pop();
-
-    queue<int> temp = q1;
-    q1 = q2;
-    q2 = temp;
+void stack_two_queues::pop() {
+  if (q1.empty()) {
+    return;
   }
 
-  void push(int num) {}
+  while (q1.size() != 1) {
+    q2.push(q1.front());
+    q1.pop();
+  }
 
-  int top() {}
-};
+  q1.pop();
+
+  queue<int> temp = q1;
+  q1 = q2;
+  q2 = temp;
+}
+
+void stack_two_queues::push(int num) { q1.push(num); }
+
+int stack_two_queues::top() {
+  while (q1.size() != 1) {
+    q2.push(q1.front());
+    q1.pop();
+  }
+
+  int top_item = q1.front();
+  q2.push(top_item);
+
+  queue<int> temp = q1;
+  q1 = q2;
+  q2 = temp;
+
+  return top_item;
+}
--- stack_two_queues/stack_two_queues.hpp
+++ stack_two_queues/stack_two_queues.hpp
@@ -1,7 +1,12 @@
 #ifndef stack_two_queues_h_
 #define stack_two_queues_h_
 
+#include <queue>
+using std::queue;
+
 class stack_two_queues {
+  queue<int> q1, q2;
+
  public:
   stack_two_queues() {}
 
