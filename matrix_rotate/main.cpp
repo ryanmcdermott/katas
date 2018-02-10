d69d5a4ea36576696951a63c2f4370f9b10fcb88
Ryan McDermott <ryantmcdermott@gmail.com>
2018-02-09 21:13:55 -0800

    Rotate matrix test

--- matrix_rotate/main.cpp
+++ matrix_rotate/main.cpp
@@ -1,7 +1,17 @@
-#include "./matrix_rotate.hpp"
 #include <assert.h>
 #include <iostream>
+#include <vector>
+#include "./matrix_rotate.hpp"
 
 using std::cout;
+using std::vector;
 
-int main() {}
+int main() {
+  vector<vector<int>> matrix;
+  matrix.push_back({1, 2, 3, 4});
+  matrix.push_back({5, 6, 7, 8});
+  matrix.push_back({9, 10, 11, 12});
+  matrix.push_back({13, 14, 15, 16});
+  matrix_rotate(matrix);
+  print_matrix(matrix);
+}
