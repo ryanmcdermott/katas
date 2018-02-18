b8ca7a9785983ac56cdec207742447dfcbc8eae2
Ryan McDermott <ryantmcdermott@gmail.com>
2018-02-18 10:54:48 -0800

    Topological sort methods

--- topological_sort/topological_sort.cpp
+++ topological_sort/topological_sort.cpp
@@ -1,9 +1,11 @@
 #include "./topological_sort.hpp"
+#include <iostream>
 #include <list>
 #include <stack>
 
 using std::list;
 using std::stack;
+using std::cout;
 
 Graph::Graph(int V) {
   this->V = V;
@@ -12,6 +14,34 @@ Graph::Graph(int V) {
 
 void Graph::addEdge(int v, int w) { adj[v].push_back(w); }
 
-void Graph::topologicalSortUtil(int v, bool visited[], stack<int> &Stack) {}
+void Graph::topologicalSortUtil(int v, bool visited[], stack<int> &to_visit) {
+  visited[v] = true;
 
-void Graph::topologicalSort() {}
+  for (list<int>::iterator i = adj[v].begin(); i != adj[v].end(); i++) {
+    if (!visited[*i]) {
+      topologicalSortUtil(*i, visited, to_visit);
+    }
+  }
+
+  to_visit.push(v);
+}
+
+void Graph::topologicalSort() {
+  stack<int> to_visit;
+  bool *visited = new bool[V];
+
+  for (int i = 0; i < V; i++) {
+    visited[i] = false;
+  }
+
+  for (int i = 0; i < V; i++) {
+    if (visited[i] == false) {
+      topologicalSortUtil(i, visited, to_visit);
+    }
+  }
+
+  while (to_visit.empty() == false) {
+    cout << to_visit.top() << " ";
+    to_visit.pop();
+  }
+}
