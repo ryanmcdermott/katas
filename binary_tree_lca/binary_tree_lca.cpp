0465c6b6f46794e0aa0fa1bfc020e6860b36dd22
Ryan McDermott <ryantmcdermott@gmail.com>
2018-02-24 11:05:20 -0800

    Binary tree lowest common ancestor methods

--- binary_tree_lca/binary_tree_lca.cpp
+++ binary_tree_lca/binary_tree_lca.cpp
@@ -1,2 +1,26 @@
 #include "./binary_tree_lca.hpp"
-void binary_tree_lca() {}
+
+struct Node* newNode(int data) {
+  struct Node* node = new Node();
+  node->data = data;
+  node->left = 0;
+  node->right = 0;
+
+  return node;
+}
+
+struct Node* binary_tree_lca(Node* root, int n1, int n2) {
+  if (root == 0) {
+    return 0;
+  }
+
+  if (root->data > n1 && root->data > n2) {
+    return binary_tree_lca(root->left, n1, n2);
+  }
+
+  if (root->data < n1 && root->data < n2) {
+    return binary_tree_lca(root->right, n1, n2);
+  }
+
+  return root;
+}
--- binary_tree_lca/binary_tree_lca.hpp
+++ binary_tree_lca/binary_tree_lca.hpp
@@ -1,6 +1,12 @@
 #ifndef binary_tree_lca_h_
 #define binary_tree_lca_h_
 
-void binary_tree_lca();
+struct Node {
+  int data;
+  struct Node *left, *right;
+};
+
+struct Node *newNode(int data);
+struct Node *binary_tree_lca(Node *root, int n1, int n2) {}
 
 #endif
