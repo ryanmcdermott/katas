9f8692828403066da769998c325aa7533a1b43a8
Ryan McDermott <ryantmcdermott@gmail.com>
2018-02-28 21:40:13 -0800

    Subset improvements

--- subset_sum/subset_sum.cpp
+++ subset_sum/subset_sum.cpp
@@ -1,2 +1,23 @@
 #include "./subset_sum.hpp"
-bool subset_sum(int set[], int n, int sum) { return true; }
+bool subset_sum(int set[], int n, int sum) {
+  bool subset[n + 1][sum + 1];
+  for (int i = 0; i <= n; i++) {
+    subset[i][0] = true;
+  }
+
+  for (int i = 0; i <= sum; i++) {
+    subset[0][i] = false;
+  }
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= sum; j++) {
+      if (j < set[i - 1]) {
+        subset[i][j] = subset[i - 1][j];
+      } else {
+        subset[i][j] = subset[i - 1][j] || subset[i - 1][j - set[i - 1]];
+      }
+    }
+  }
+
+  return subset[n][sum];
+}
