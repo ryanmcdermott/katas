5aefa38e582436c7d6b8a21c68141e38eeae31a1
Ryan McDermott <ryantmcdermott@gmail.com>
2018-03-01 20:17:06 -0800

    Subset test

--- subset_sum/main.cpp
+++ subset_sum/main.cpp
@@ -1,7 +1,11 @@
-#include "./subset_sum.hpp"
 #include <assert.h>
 #include <iostream>
+#include "./subset_sum.hpp"
 
 using std::cout;
 
-int main() {}
+int main() {
+  int set[] = {2, 3, 5, 7, 20};
+  int n = sizeof(set) / sizeof(set[0]);
+  bool isSubset = subset_sum(set, n, 28);
+}
