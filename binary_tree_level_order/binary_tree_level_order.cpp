ff0dedb173a154e7a4af49a95eb66351e4428e8d
Ryan McDermott <ryantmcdermott@gmail.com>
2018-03-06 20:02:19 -0800

    Binary tree level order more stubs

--- binary_tree_level_order/binary_tree_level_order.cpp
+++ binary_tree_level_order/binary_tree_level_order.cpp
@@ -1,4 +1,5 @@
 #include "./binary_tree_level_order.hpp"
+#include <vector>
 
 struct Node* newNode(int data) {
   struct Node* node = new Node();
@@ -7,6 +8,23 @@ struct Node* newNode(int data) {
   node->right = 0;
 
   return node;
+}
+
+void print_level(Node* root, int level) {}
+
+int height(Node* root) {
+  if (root == 0) {
+    return 0;
+  } else {
+    int left_height = height(root->left);
+    int right_height = height(root->right);
+
+    if (left_height > right_height) {
+      return left_height + 1;
+    }
+
+    return right_height + 1;
+  }
 }
 
 void binary_tree_level_order() {}
